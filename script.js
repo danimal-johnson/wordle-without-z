77c64bdcbee530b2e1c2c6aa10dfe4182c7eb1eb
Dan Johnson <danimal.johnson@gmail.com>
2022-03-20 05:29:41 -0700

    Import lists. Implement mouse and keyboard inputs. Import dictionary and wordlist from original Wordle game. Control in-game keyboard with user's keyboard and mouse.

--- script.js
+++ script.js
@@ -1,28 +1,76 @@
+
+// wordlist and dictionary are included in the HTML
 const cells = document.querySelectorAll('.cell');
 
-function handleClick(e) {
-
-  const cell = e.target;  
-  let state = cell.getAttribute('data-state');
-
-  if (state === 'active') {
-    cell.innerText = 'X';
-    cell.backgroundColor = 'red';
-    cell.removeAttribute('data-state');
-    // TODO: Change the state of the next cell to active
+function handleKeyPress(e) {
+  const key = e.key;
+  if (key === 'Enter') {
+    submitGuess(key.toUpperCase());
+    return;
   }
-
-
-  // const key = cell.getAttribute('data-key');
-  
-  // if (cellValue === '') {
-  //   cell.innerText = 'X';
-  //   cell.classList.add('x');
-  //   cell.classList.remove('o');
-  //   cell.classList.remove('empty');
-  //   cell.classList.add('filled');
-  //   checkWinner(cellIndex, cellRow, cellCol, cellDiag);
-
+  if (key === 'Backspace' || key === 'Delete') {
+    deleteLastLetter();
+    return;
+  }
+  pressKey(key);
 }
 
-cells.forEach(cell => cell.addEventListener('click', handleClick));
+function handleClick(e) {
+  if (e.target.matches('[data-key]')) {
+    const key = e.target.dataset.key;
+    pressKey(key);
+    return;
+  }
+  if (e.target.matches('[data-enter]')) {
+    submitGuess();
+    return;
+  }
+  if (e.target.matches('[data-backspace]')) {
+    deleteLastLetter();
+    return;
+  }
+}
+
+function pressKey(key) {
+  // TODO: Add the letter to the guess and update the display
+  console.log('Receiving key:', key);
+  return;
+}
+function submitGuess() {
+  // TODO: Check if the guess is correct
+  console.log('Submitting...');
+  return;
+}
+function deleteLastLetter() {
+  // TODO: Delete the last letter from the guess
+  console.log('Deleting last letter...');
+  return;
+}
+
+function updateGameBoard() {
+  console.log('Updating game board...');
+  // TODO: Update the game board
+  return;
+  // const cell = e.target;  
+  // let state = cell.getAttribute('data-state');
+
+  // if (state === 'active') {
+  //   cell.innerText = 'X';
+  //   cell.backgroundColor = 'red';
+  //   cell.removeAttribute('data-state');
+  // }
+}
+
+function startPlay() {
+  document.addEventListener('keydown', handleKeyPress);
+  document.addEventListener('click', handleClick);
+}
+
+function endPlay() {
+  document.removeEventListener('keydown', handleKeyPress);
+  document.removeEventListener('click', handleClick);
+}
+
+
+
+startPlay();
