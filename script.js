f7f6ef586419de57766371911b51868978fa2458
Dan Johnson <danimal.johnson@gmail.com>
2022-03-20 16:56:37 -0700

    Update game board with keyboard input.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 
 // wordlist and dictionary are included in the HTML
-const cells = document.querySelectorAll('.cell');
+const guessGrid = document.querySelector('[data-guess-grid]');
 
 function handleKeyPress(e) {
   const key = e.key;
   if (key === 'Enter') {
-    submitGuess(key.toUpperCase());
+    submitGuess();
     return;
   }
   if (key === 'Backspace' || key === 'Delete') {
     deleteLastLetter();
     return;
   }
-  pressKey(key);
+  if (key.match(/^[a-zA-Z]$/)) {
+    pressKey(key);
+  }
 }
 
 function handleClick(e) {
@@ -31,21 +33,56 @@ function handleClick(e) {
   }
 }
 
+function getActiveTiles() {
+  // Returns an array of all unsubmitted tiles on a line.
+  return guessGrid.querySelectorAll('[data-state="active"]');
+}
+
 function pressKey(key) {
-  // TODO: Add the letter to the guess and update the display
+  // Adds the letter to the guess and update the display
   console.log('Receiving key:', key);
-  return;
+  const activeTiles = getActiveTiles();
+  if (activeTiles.length >= 5) return;
+
+  const nextTile = guessGrid.querySelector(':not([data-letter])');
+  nextTile.dataset.letter = key.toLowerCase();
+  nextTile.textContent = key.toUpperCase();
+  nextTile.dataset.state = 'active';
 }
 function submitGuess() {
   // TODO: Check if the guess is correct
+  const correctWord = 'hello';
+  let submittedWord = "";
+  const activeTiles = getActiveTiles();
+  if (activeTiles.length < 5) return;
+  
   console.log('Submitting...');
+  for (tile of activeTiles) {
+    submittedWord += tile.dataset.letter;
+  }
+  if (submittedWord === correctWord) {
+    console.log('Correct!');
+    // Celebrate win
+  } else {
+    // Set next tile to active
+    // Set all previous tiles to inactive
+    // Decorate the current line with colors
+  }
+
   return;
 }
 function deleteLastLetter() {
-  // TODO: Delete the last letter from the guess
+  // Delete the last letter from the guess (current row only)
   console.log('Deleting last letter...');
-  return;
+  const activeTiles = getActiveTiles();
+  if (activeTiles.length === 0) return;
+
+  const lastTile = activeTiles[activeTiles.length - 1];
+  lastTile.removeAttribute('data-letter');
+  lastTile.textContent = '';
+  lastTile.dataset.state = 'active';
 }
+
 
 function updateGameBoard() {
   console.log('Updating game board...');
@@ -70,7 +107,5 @@ function endPlay() {
   document.removeEventListener('keydown', handleKeyPress);
   document.removeEventListener('click', handleClick);
 }
-
-
 
 startPlay();
