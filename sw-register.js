bc4965160f28383c6e87e89135e0dc99dce18740
Dan Johnson <danimal.johnson@gmail.com>
2022-03-31 21:29:29 -0700

    Service worker updates. Enable service worker.

--- sw-register.js
+++ sw-register.js
@@ -1,16 +1,10 @@
 if ('serviceWorker' in navigator)
   navigator.serviceWorker.register('serviceworker.js');
 
-// TODO: Add <script src="sw-register.js"></script> to index.html
-// Update colors in index.html
-// Update colors in app.webmanifest
+// TODO:
 // Consider adding 'beforeInstallPrompt' event listener to index.js
-// Create icons for Android and iOS
-// Add @media (display-mode: standalone) to index.css
 // Turn off user-select for things like buttons.
-
-
-
+// Maybe:
 // Add <meta name="apple-mobile-web-app-capable" content="yes"> to index.html?
 // Add <meta name="apple-mobile-web-app-status-bar-style" content="black"> to index.html?
 // Add <meta name="apple-mobile-web-app-title" content="My App"> to index.html?
