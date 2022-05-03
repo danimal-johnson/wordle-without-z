6b72b454b0787d942c784eab338f4eb28b45c572
Dan Johnson <danimal.johnson@gmail.com>
2022-05-03 07:11:38 -0700

    Improve scaling for small devices fix: Allow grid to shrink on smaller displays fix: Take into account unusable display space on mobile devices (set vh manually) fix: Reduce display height on Safari mobile screens with secret -webkit-fill-available feature

--- app.js
+++ app.js
@@ -375,6 +375,15 @@ document.querySelector('.install-button').addEventListener('click', e => {
   }
 });
 
+// ----- Adjust screen sizing for mobile -----
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 // ----- Start the game -----
 startInteraction();
 
